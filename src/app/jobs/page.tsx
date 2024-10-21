24a5bdadb31a4acc18c4ceeb49b7d19fbd039982
Muhammad Nasir <muhammadnasir8787914@gmail.com>
2024-10-22 00:35:17 +0500

    change in job page

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,14 +1,63 @@
-export default function Jobs () {
-    throw new Error ("job page not yet implemented");
+// pages/404.js
+import Link from 'next/link';
+import Errorpage from '@/app/error'; // Import your Errorpage component
+
+export default function Custom404() {
+  return (
+    <Errorpage /> // Render the Errorpage component directly
+  );
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export default function Jobs () {
+//     throw new Error ("job page not yet implemented");
     
-    return (
-        <div>
+//     return (
+//         <div>
 
             
 
-            <h1> <p className="home-background text-black"> We connect talented individuals with exciting job opportunities in the tech industry. Our platform features a range of positions, from entry-level to experienced roles, across various sectors. Explore the latest job listings and take the next step in your career with us!
-            </p></h1>
+//             <h1> <p className="home-background text-black"> We connect talented individuals with exciting job opportunities in the tech industry. Our platform features a range of positions, from entry-level to experienced roles, across various sectors. Explore the latest job listings and take the next step in your career with us!
+//             </p></h1>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
